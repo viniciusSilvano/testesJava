2f63e3f6ca5132516b20a18b0bbf6ffd80826337
Vinicius Silvano <viniciusssf3@gmail.com>
2020-03-14 21:40:18 -0300

    Preparando para retornar os livros pelo rest.

--- javaTesteProject/src/main/java/br/com/testeJava/rest/LivroRest.java
+++ javaTesteProject/src/main/java/br/com/testeJava/rest/LivroRest.java
@@ -1,12 +1,18 @@
 package br.com.testeJava.rest;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import br.com.testeJava.bo.LivroService;
+import br.com.testeJava.dto.LivroDto;
 
 @Path("/livro")
 public class LivroRest {
@@ -16,8 +22,10 @@ public class LivroRest {
 	
 	@GET
 	@Path(value = "")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getLivro() {
-		return Response.ok().build();
+		List<LivroDto> livros = livroService.consultarLivros();
+		return Response.status(Status.OK).entity(livros).build();
 	}
 	
 	@GET
