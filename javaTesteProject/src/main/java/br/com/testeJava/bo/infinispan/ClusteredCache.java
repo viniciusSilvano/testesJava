9a1e13203bcd52c1fb0a37b42a5a60f77814079f
Vinícius Silvano Silveira Felipe <vinic@192.168.79.145>
2020-07-29 13:56:04 -0300

    alterando teste infinispan.

--- javaTesteProject/src/main/java/br/com/testeJava/bo/infinispan/ClusteredCache.java
+++ javaTesteProject/src/main/java/br/com/testeJava/bo/infinispan/ClusteredCache.java
@@ -1,5 +1,7 @@
 package br.com.testeJava.bo.infinispan;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
@@ -8,6 +10,10 @@ import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.transaction.LockingMode;
+import org.infinispan.transaction.TransactionMode;
+import org.infinispan.transaction.lookup.JBossStandaloneJTAManagerLookup;
+import org.infinispan.util.concurrent.IsolationLevel;
 
 @Startup
 @Singleton
@@ -16,15 +22,20 @@ public class ClusteredCache extends CacheManager {
 	@Override
 	protected GlobalConfiguration getGlobalConfiguration() {
 		return GlobalConfigurationBuilder
-				.defaultClusteredBuilder()
-				.globalJmxStatistics()
-				.allowDuplicateDomains(true)
-				.enable().build();				
+				.defaultClusteredBuilder().build();				
 	}
 
 	@Override
 	protected Configuration getConfiguration() {
-		return new ConfigurationBuilder().clustering().cacheMode(CacheMode.REPL_ASYNC).build();
+		return new ConfigurationBuilder().clustering().cacheMode(CacheMode.REPL_SYNC)
+				.locking()
+					.lockAcquisitionTimeout(40,TimeUnit.MINUTES)
+					.isolationLevel(IsolationLevel.REPEATABLE_READ)
+				.transaction()
+					.transactionMode(TransactionMode.TRANSACTIONAL)
+					.lockingMode(LockingMode.OPTIMISTIC)
+					.transactionManagerLookup(new JBossStandaloneJTAManagerLookup())
+				.build();
 	}
 	
 }
