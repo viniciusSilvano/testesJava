23f678899a03798e13a6ccd358fe7af1ba23e913
Vinicius Felipe <viniciusssf3@gmail.com>
2020-12-09 23:48:26 -0300

    Tornando o Colaborador uma entidade na conexão recém criada.

--- javaTesteProject/src/main/java/br/com/testeJava/entity/pessoa/Colaborador.java
+++ javaTesteProject/src/main/java/br/com/testeJava/entity/pessoa/Colaborador.java
@@ -2,12 +2,33 @@ package br.com.testeJava.entity.pessoa;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import br.com.testeJava.entity.profissoes.Profissao;
 
-
+@Entity
+@PersistenceUnit(name = "javaTesteProject")
+@Table(name = "TB_PESSOA")
 public class Colaborador implements Pessoa {
+	@Id
+	@GeneratedValue
+	@Column(name = "ID_PESSOA")
 	private Long id;
+	
+	@Transient
 	private List<Profissao> profissoes;
+	
+	@NotNull
+	@Size(min = 1, max = 255)
+	@Column(name = "NOME_PESSOA")
 	private String nome;
 
 	public Colaborador() {}
